2c258b294150a0e6fbbf72fb05b159c190081c7e
Pâmela <pamela.potenciatech@gmail.com>
2021-05-11 21:50:04 -0300

    test js

---
+++ index.js
@@ -0,0 +1,30 @@
+// Crie uma variável que receba seu nome;
+
+// Crie uma variável que receba sua idade;
+
+// Crie uma variável com o ano atual e subtraia pela sua idade e mostre no console a mensagem "Meu ano de nascimento é...";
+
+// Crie uma variável chamada souhumano e atribua à ela o valor booleano que representa verdadeiro;
+
+// Crie duas variáveis e atribua a cada uma um número;
+
+// Agora mostre no console o resultado da soma das duas variáveis;
+
+let name = `Pâmela`
+console.log(`Hello, mi nombre es ${name}`)
+
+let age = 23
+console.log(`Yo tengo ${age} años`)
+
+let year = 2021 - 23
+console.log(`Meu ano de nascimento é ${year}`)
+
+let iamHuman = true
+ 
+if(iamHuman){
+    console.log(`Sou humano não consigo ser perfeito`)
+}
+
+let n1 = 3
+let n3 = 7
+console.log(3+7)
